ad6bf15e41d9ba61043fae83f73b3352e03af1f1
johan cazier <45624266+JohanCaz@users.noreply.github.com>
2023-03-25 16:45:30 +0100

    commit johan

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser')
+var cors = require('cors')
 const app = express();
 const port = 3000;
 const initAllRoutes = require('./routes');
-
+app.use(cors())
 app.use(bodyParser.urlencoded({ extended: true }));
 app.get('/', (req, res) => {
   res.send('Hello World!')
